179bf7e8687aee53e2537bc63235bb60056db839
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2026-08-12 15:29:57 +0100

    feat: update login template to use heading component for consistency (#94)

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,5 +6,10 @@ from django.contrib.auth.forms import AuthenticationForm
 class GdsAuthForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs.pop("autofocus", None)
+        self.fields["username"].error_messages["required"] = "Enter your email address"
+        self.fields["password"].error_messages["required"] = "Enter your password"
+
         self.helper = FormHelper()
         self.helper.layout = Layout("username", "password", Button("submit", "Sign in"))
