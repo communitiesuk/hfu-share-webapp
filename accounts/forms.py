47ccdf7e829f73fd1aa47ed4ee3ba59ec282dc1e
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2026-08-12 15:09:51 +0100

    fix: refactor login template structure and simplify form layout

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from crispy_forms_gds.helper import FormHelper
-from crispy_forms_gds.layout import Button, Field, Fluid, Layout
+from crispy_forms_gds.layout import Button, Layout
 from django.contrib.auth.forms import AuthenticationForm
 
 
@@ -7,8 +7,4 @@ class GdsAuthForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.layout = Layout(
-            Field.text("username", field_width=Fluid.ONE_HALF),
-            Field.text("password", field_width=Fluid.ONE_HALF),
-            Button("submit", "Sign in"),
-        )
+        self.helper.layout = Layout("username", "password", Button("submit", "Sign in"))
