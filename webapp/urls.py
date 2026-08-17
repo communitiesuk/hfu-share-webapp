120e42a871937c2f8f34d1f84c24cd194791551b
Tim South <58297459+tim-s-ccs@users.noreply.github.com>
2026-08-17 12:41:09 +0000

    fix: Remove old dedupe code (#108)

--- webapp/urls.py
+++ webapp/urls.py
@@ -19,21 +19,6 @@ urlpatterns = [
         views.LandingPageView.as_view(),
         name="landing-page",
     ),
-    path(
-        "review-potential-duplicate-records",
-        views.PotentialDuplicateListView.as_view(),
-        name="review-potential-duplicate-records",
-    ),
-    path(
-        "review-potential-duplicate-records/guests/suggested",
-        views.PotentialDuplicateGuestSuggestedListView.as_view(),
-        name="review-duplicate-guest-records-suggested",
-    ),
-    path(
-        "review-potential-duplicate-records/guests/manual",
-        views.PotentialDuplicateGuestManualListView.as_view(),
-        name="review-duplicate-guest-records-manual",
-    ),
     path("csp-report/", views.CSPReportView.as_view(), name="csp-report"),
     path("favicon.ico", views.favicon_redirect, name="favicon_redirect"),
 ]
