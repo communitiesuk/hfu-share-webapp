3b9f39bc5efee30824d6ffe4472169ae6741486f
tim-s-ccs <Timothy.South@communities.gov.uk>
2026-08-20 16:12:11 +0100

    fix: add reference to comment in ticket

--- browser_tests/pages/home_page.py
+++ browser_tests/pages/home_page.py
@@ -2,7 +2,7 @@ from .share_page import SharePage
 
 
 class HomePage(SharePage):
-    # TODO: Replace when we have the card updated
+    # TODO: Replace when we have the card updated HFURB-3979
     def click_on_card(self, card_text):
         self.page.get_by_role("heading", level=3, name=card_text).locator(
             "xpath=../../../../a"
