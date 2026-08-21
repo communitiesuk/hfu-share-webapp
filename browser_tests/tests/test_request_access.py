e586ed3d4788c0a2e5847603f70516344e06c13f
Rich Neat <rich.neat@madetech.com>
2026-08-21 12:08:47 +0100

    feat: update request link navigation to use deterministic value

--- browser_tests/tests/test_request_access.py
+++ browser_tests/tests/test_request_access.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+
 from .base import BrowserTest
 
 
 class TestRequestAccessJourney(BrowserTest):
     def test_request_access_journey_and_pending_status(self, home_page):
+        reason_for_access = f"Browser testing {datetime.now()}"
+
         home_page.click_link("Request access")
         home_page.assert_has_heading("Request access to data")
 
@@ -14,7 +18,7 @@ class TestRequestAccessJourney(BrowserTest):
         home_page.assert_has_heading("Tell us why you need access")
 
         home_page.enter_text_into_form_field(
-            "Reason for requesting access", "Browser testing"
+            "Reason for requesting access", reason_for_access
         )
         home_page.click_button("Next")
         home_page.assert_has_heading("Check your answers")
@@ -26,7 +30,10 @@ class TestRequestAccessJourney(BrowserTest):
         home_page.assert_has_heading("Welcome")
         home_page.assert_page_contains_text("Pending requests")
 
-        home_page.click_link("Home Office")
+        home_page.click_link(
+            "Home Office",
+            element=home_page.main_page.locator("tr", has_text=reason_for_access),
+        )
         home_page.assert_has_heading("Your request")
 
         home_page.assert_page_contains_text("Request date")
@@ -34,4 +41,4 @@ class TestRequestAccessJourney(BrowserTest):
         home_page.assert_page_contains_text("User group")
         home_page.assert_page_contains_text("Home Office operations team")
         home_page.assert_page_contains_text("Tell us why access is needed")
-        home_page.assert_page_contains_text("Browser testing")
+        home_page.assert_page_contains_text(reason_for_access)
