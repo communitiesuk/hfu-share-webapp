95b16377a4d7b60db7c2934e30fa4c3a526f66e4
Tim South <58297459+tim-s-ccs@users.noreply.github.com>
2026-08-21 09:55:29 +0000

    fix: browser tests look within main by default (#135)

--- browser_tests/pages/home_page.py
+++ browser_tests/pages/home_page.py
@@ -4,6 +4,6 @@ from .share_page import SharePage
 class HomePage(SharePage):
     # TODO: Replace when we have the card updated HFURB-3979
     def click_on_card(self, card_text):
-        self.page.get_by_role("heading", level=3, name=card_text).locator(
+        self.main_page.get_by_role("heading", level=3, name=card_text).locator(
             "xpath=../../../../a"
         ).click()
