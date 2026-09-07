a69154a6751677e2dbce8d10d54ca9ef5302741c
Tim South <58297459+tim-s-ccs@users.noreply.github.com>
2026-09-07 15:38:33 +0000

    chore: add browser test user to the users created in the seeder script (#170)

--- hfurb_scripts/seeders/stages/seed_custom_users.py
+++ hfurb_scripts/seeders/stages/seed_custom_users.py
@@ -1,13 +1,51 @@
 import os
+from typing import TypedDict
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.db import transaction
 
+from accounts.models import User as UserModel
+
+
+class UserData(TypedDict):
+    email: str
+    group_name: str
+
+
+def seed_user(user_data: UserData, password: str, user_model: UserModel):
+    email = user_data["email"]
+    group_name = user_data["group_name"]
+    username = email.split("@", maxsplit=1)[0]
+
+    # Create or get the user
+    user, created = user_model.objects.get_or_create(
+        email=email,
+        username=username,
+        is_staff=False,
+        is_superuser=False,
+    )
+
+    user.set_password(password)
+    user.save()
+
+    if created:
+        print(f"Created user {email}")
+    else:
+        print(f"Updated user {email}")
+
+    # Add user to the specified group
+    group = Group.objects.get(name=group_name)
+    group.user_set.add(user)  # type: ignore[attr-defined]
+
+    print(f"Added user {email} to group {group_name}")
+
 
 def seed_custom_users():
     User = get_user_model()
     password = os.environ.get("LOCAL_USER_PASSWORD")
+    browser_test_user_email = os.environ.get("BROWSER_TEST_USER_EMAIL")
+    browser_test_user_password = os.environ.get("BROWSER_TEST_USER_PASSWORD")
 
     users_to_create = [
         {
@@ -42,30 +80,30 @@ def seed_custom_users():
 
     with transaction.atomic():
         for user_data in users_to_create:
-            email = user_data["email"]
-            group_name = user_data["group_name"]
-            username = email.split("@", maxsplit=1)[0]
+            seed_user(user_data, password, User)
 
-            # Create or get the user
-            user, created = User.objects.get_or_create(
-                email=email,
-                username=username,
-                is_staff=False,
-                is_superuser=False,
+        if browser_test_user_email and browser_test_user_password:
+            seed_user(
+                {
+                    "email": browser_test_user_email,
+                    "group_name": "ltla_hobbiton_browser_test",
+                },
+                browser_test_user_password,
+                User,
             )
-
-            user.set_password(password)
-            user.save()
-
-            if created:
-                print(f"Created user {email}")
-            else:
-                print(f"Updated user {email}")
-
-            # Add user to the specified group
-            group = Group.objects.get(name=group_name)
-            group.user_set.add(user)
-
-            print(f"Added user {email} to group {group_name}")
+        else:
+            print("Skipping creation of browser test user")
+            for key, value in (
+                (
+                    "BROWSER_TEST_USER_EMAIL",
+                    browser_test_user_email,
+                ),
+                (
+                    "BROWSER_TEST_USER_PASSWORD",
+                    browser_test_user_password,
+                ),
+            ):
+                if not value:
+                    print(f"ENV variable {key} was not set")
 
     print("Custom users seeding completed.")
