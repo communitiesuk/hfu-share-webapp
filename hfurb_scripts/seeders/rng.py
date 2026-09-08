a7da1aa30b306c527fed00b1413432b0584fca9b
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2026-09-08 14:04:20 +0000

    fix: stop the browser test seeder importing test_utils, which is not in the image (#177)

--- hfurb_scripts/seeders/rng.py
+++ hfurb_scripts/seeders/rng.py
@@ -1,6 +1,5 @@
 import random
 
-import factory.random
 from faker import Faker
 
 rng = random.Random()
@@ -8,10 +7,5 @@ fake = Faker("en_GB")
 
 
 def seed_seeders(seed: int) -> None:
-    from test_utils.faker import fake as factory_fake
-
     rng.seed(seed)
     fake.seed_instance(seed)
-    factory_fake.seed_instance(seed)
-    factory_fake.unique.clear()
-    factory.random.reseed_random(seed)
