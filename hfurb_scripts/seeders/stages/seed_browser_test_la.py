f8c014be998eaa1cba04635d30359cf4a01d1627
Manny Dinssa <44172848+Dinssa@users.noreply.github.com>
2026-09-10 14:39:55 +0100

    feat: add MvAccommodation to deduplication principals and ID array fields

--- hfurb_scripts/seeders/stages/seed_browser_test_la.py
+++ hfurb_scripts/seeders/stages/seed_browser_test_la.py
@@ -842,6 +842,7 @@ SEEDED_ID_START = f"{BROWSER_TEST_ID_PREFIX}-"
 DEDUPLICATION_PRINCIPALS = [
     (MvPerson, "person", ["guestduplicategroup__guests"]),
     (MvVolunteer, "sponsor", ["sponsorduplicategroup__sponsors"]),
+    (MvAccommodation, "accommodation", ["accommodationduplicategroup__accommodations"]),
 ]
 
 RECORDS_LINKED_TO_SEEDED_RECORDS = [
@@ -868,6 +869,7 @@ ID_ARRAY_FIELDS = {
         (MvAccommodationRequest, "sponsor_id"),
         (MvAccommodationRequest, "sponsor_withdrawn"),
     ],
+    MvAccommodation: [(MvAccommodationRequest, "accommodation_id")],
 }
 
 
